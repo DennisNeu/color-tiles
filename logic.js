76b0c5f23490f334ffacd96f4dfc68aabba7be09
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2024-06-20 19:14:38 +0200

    Add more colors, fixed color switching bug, footer
    Previously, pressing a tile could result in it
    being assigned the same color. This has been
    changed. Now each button press results in a new
    color.

--- logic.js
+++ logic.js
@@ -1,14 +1,19 @@
 const container = document.querySelector(".main-container");
 
 function getRandomColor() {
-    const colors =  ["blue", "red", "green", "yellow", "white", "pink", "black", "purple", "brown"];
+    const colors =  ["blue", "red", "green", "yellow", "white", "pink", "black", "purple", "brown", "blueviolet", "chartreuse", "cyan", "crimson", "darkblue", "darkmagenta", "darkred", "deeppink"];
     const randomNumber = Math.round(Math.random() * (colors.length - 1));
     console.log(randomNumber);
     return colors[randomNumber];
 }
 
 function changeColor() {
-    this.style.backgroundColor = getRandomColor();
+    const currentColor = this.style.backgroundColor;
+    let newColor = getRandomColor();
+    while(newColor === currentColor) {
+        newColor = getRandomColor();
+    }
+    this.style.backgroundColor = newColor;
 }
 
 for (i = 0; i < 10; i++) {
